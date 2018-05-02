ebc977bc956ad6d75f84dd744685b9663365023a
chelovecheki <36590480+chelovecheki@users.noreply.github.com>
2018-05-03 00:12:40 +0700

    Начало работы над НТВП; Созданы классы, интерфейс и добавлено консольное приложение

---
+++ Model/Model/Uniformly.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    // Равномерное движение
+    class Uniformly : IMovement
+    {
+        private double _initialCoordinate;
+        private int _time;
+        private double _speed;
+
+        //public Uniformly(double initialCoordinate, int time, double speed)
+        //{
+        //    _initialCoordinate = initialCoordinate;
+        //    _time = time;
+        //    _speed = speed;
+        //}
+
+        public int Time
+        {
+            get { return _time; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректное время");
+                }
+                _time = value;
+            }
+        }
+
+        public double Speed
+        {
+            get { return _speed; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректная скорость");
+                }
+                _speed = value;
+            }
+        }
+
+        public double CalculateNewCoordinate(double initialCoordinate, int time, double speed)
+        {
+            double newCoordinate = initialCoordinate + time * speed;
+            return newCoordinate;
+        }
+    }
+}
---
+++ Model/Model/UniformlyAccelerated.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class UniformlyAccelerated
+    {
+        private double _initialCoordinate;
+        private int _time;
+        private double _initialSpeed;
+        private double _acceleration;
+
+        public int Time
+        {
+            get { return _time; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректное время");
+                }
+                _time = value;
+            }
+        }
+
+        public double Speed
+        {
+            get { return _initialSpeed; }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Некорректная скорость");
+                }
+                _initialSpeed = value;
+            }
+        }
+
+        public double CalculateNewCoordinate(double initialCoordinate, int time, double initialSpeed, double acceleration)
+        {
+            double newCoordinate = initialCoordinate + initialSpeed * time + 1/2 * acceleration * time * time;
+            return newCoordinate;
+        }
+    }
+}
